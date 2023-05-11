b793f6f257a2e7b8105de53fbd46562c8fbc489d
QuityQ <biciciayberk@gmail.com>
2023-05-12 01:08:35 +0200

    created multiple function for db

--- classes/database.py
+++ classes/database.py
@@ -1,12 +1,92 @@
 import psycopg2
 import pandas as pd
+from sqlalchemy import create_engine
 
-
-connection = psycopg2.connect(user="bicicayb",
-                              password="sIrAKRSC",
-                              host="db-moviedata.cxqntpnwnjaq.eu-north-1.rds.amazonaws.com",
-                              port="5432",
-                              database="postgres")
-
+connection = psycopg2.connect(
+    user="bicicayb",
+    password="sIrAKRSC",
+    host="db-moviedata.cxqntpnwnjaq.eu-north-1.rds.amazonaws.com",
+    port="5432",
+    database="postgres",
+)
 cursor = connection.cursor()
+engine = create_engine(
+    "postgresql+psycopg2://bicicayb:sIrAKRSC@db-moviedata.cxqntpnwnjaq.eu-north-1.rds.amazonaws.com:5432/postgres"
+)
 
+
+def testConnection():
+    try:
+        cursor.open()
+        print(connection.get_dsn_parameters(), "\n")
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record, "\n")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while connecting to DB", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
+# todo take df as argument
+def uploadMovieData(df):
+    try:
+        df.to_sql("movies", engine, if_exists="append", index=False)
+        print("Data uploaded")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while uploading data", error)
+    finally:
+        if connection:
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
+def createMoviesTable():
+    try:
+        create_table_query = """CREATE TABLE IF NOT EXISTS movies
+              (id SERIAL PRIMARY KEY,
+              title TEXT,
+              budget INT,
+              genres TEXT,
+              original_language TEXT,
+              original_title TEXT,
+              popularity FLOAT,
+              production_companies TEXT,
+              production_countries TEXT,
+              release_date TEXT,
+              revenue INT,
+              runtime INT,
+              spoken_languages TEXT,
+              status TEXT,
+              vote_average FLOAT,
+              vote_count INT,
+              adult BOOLEAN); """
+        cursor.open()
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table created")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while creating table", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+def dropMoviesTable():
+    try:
+        cursor.open()
+        drop_table_query = """DROP TABLE movies"""
+        cursor.execute(drop_table_query)
+        connection.commit()
+        print("Table dropped")
+    except (Exception, psycopg2.Error) as error:
+        print("Error while dropping table", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
